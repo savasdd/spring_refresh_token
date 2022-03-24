e33d352c086511d4bbab771e2bfd031df27fbc6b
Savaş Dede <savas.dede@ikmbilisim.com.tr>
2022-03-24 10:16:51 +0300

    init

--- src/main/java/com/token/util/EnumUtil.java
+++ src/main/java/com/token/util/EnumUtil.java
@@ -4,8 +4,8 @@ public class EnumUtil {
 
 	public static final long TOKEN_EXPIRE_TIME = 90000; // :2sn
 	public static final long TOKEN_REFRESH_EXPIRE_TIME = 5_900_000; // 150000;
-	public static final String SECRET_KEY = "RXRrYjIwMjFNUFlTU3ZzRGQ/QXNrITU2SUtN";
-	public static final String SECRET_KEY_REFRESH = "RXRrYjIwMjFNUFlTLy9TdnNSRlJTVEtOK";
+	public static final String SECRET_KEY = "svsdd_refresh_token";
+	public static final String SECRET_KEY_REFRESH = "svsdd_refresh_token";
 	public static final String EXPIRED_TOKEN = "Geçerlilik Tarihi Sona Ermiş Token !";
 
 }
